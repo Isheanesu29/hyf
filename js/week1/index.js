32a755d7fc826ba2c18f3729f40969534ea5eb9e
Isheanesu Murwira <imurwira@yahoo.com>
2019-04-13 18:37:37 +0200

    updated my home work

--- js/week1/index.js
+++ js/week1/index.js
@@ -1,5 +1,158 @@
-const nr1 = 4
-const nr2 = 5
 
-console.log('nr1:' + nr1 + ' ' + 'nr2:' + nr2)
-console.log(`nr1: ${nr1}`)
+// answer to number 1 is below
+ console.log ('hello world');
+console.log ('Makadini');
+console.log ('hej,världen');
+
+// answer to number 2 is below. 
+ console.log('Im awesome')  
+
+// answer to number 3 is below 
+ let x;
+console.log('the value of x will be:undedined');
+console.log(x);
+let x = 12;
+ console.log('x the value will be:12');
+ console.log(x); 
+  
+// answer to number 4 is below;
+ let y = "I am";
+console.log('the value of my string will be: whateverYouThinkItWillL');
+console.log(y);
+let y = "Yes me";
+console.log('the value of my string will be: whateverYouThinkItWillL');
+console.log( y);
+
+//answer to number 5
+ let z = 7.25;
+console.log(z);
+let a = Math.round(7.25);
+console.log(a); 
+
+// answer to number 6
+ let names = [];
+console.log(the value of my Array will be: undefined);
+console.log(names);
+let animals = ['pigs','cats', 'rats'];
+console.log(animals);
+animals.push [('baby pig')];
+console.log(animals); 
+
+//number answer to number 7
+let myStringLength = 0;
+let myString = "this is a test";
+
+myStringLength = myString.length;
+
+
+//answer to number 8
+
+
+// answer to number 9 
+ let x = 7;
+x = 7 % 3;
+const x = 7 % 3;
+ console.log (x); 
+ x = 7 / 3;
+console.log(x); 
+
+ // answer to number 10
+ let people = ['Ishe', 29, 'Jeff', 40];
+ let x = 6/0;
+ let b = 10/0;
+ console.log('==', x === b);
+ console.log('!=', x != b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
